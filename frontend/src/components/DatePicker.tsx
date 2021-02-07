198d64bec745a82e94d938862338544fbca4788f
willfeliz1 <william_mfelizardo@hotmail.com>
2021-02-07 14:00:30 -0300

    fix: componente de calendario

--- frontend/src/components/DatePicker.tsx
+++ frontend/src/components/DatePicker.tsx
@@ -1,8 +1,11 @@
 import { useField, useFormikContext } from 'formik';
-import DatePicker from 'react-datepicker';
+import DatePicker, { registerLocale } from 'react-datepicker';
 import { createGlobalStyle } from 'styled-components';
 
 import "react-datepicker/dist/react-datepicker.css";
+
+import ptBR from 'date-fns/locale/pt-BR';
+registerLocale('ptBR', ptBR);
 
 const DatePickerWrapperStyles = createGlobalStyle`
     .date_picker.full-width {
@@ -14,18 +17,20 @@ export const DatePickerField = ({ ...props }: any) => {
   const { setFieldValue } = useFormikContext();
   const [field] = useField(props);
 
+
   return (
     <>
       <DatePicker 
         {...field}
         {...props}
-        selected={(field.value && new Date(field.value)) || null}
-        dateFormat="dd/MM/yyyy"
+        selected={(field.value && new Date(field.value))|| null}
         placeholderText="Sua data de nascimento"
         onChange={val => {
           setFieldValue(field.name, val);
         }}
         wrapperClassName='date_picker full-width'
+        locale="ptBR"
+        dateFormat='dd/MM/yyyy'
       />
       <DatePickerWrapperStyles />
     </>
